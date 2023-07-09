b187ee2a6be036990d781551357d95036931f1ed
Nico Govindsamy <ngovindsamy18@gmail.com>
2023-07-09 19:50:55 +0200

    Added terminal show board function

--- quarto.py
+++ quarto.py
@@ -1,11 +1,39 @@
 import numpy as np
-import quarto_util as util
+import quarto_util as qutil
 
 class QuartoGame:
-    def __init__(self):
+    def __init__(self, gui_mode=True):
         self.board = np.full((4,4), 16)
+        self.gui_mode = gui_mode
 
     def setBoard(self, board_encoding=None):
         #no board encoding passed, therefore clear board
         if board_encoding is None:
-            self.board = [[16]*4]*4
+            self.board = np.full((4,4), 16)
+        else:
+            self.board = qutil.decodeBoard(board_encoding)
+    
+    def showBoard(self):
+        if self.gui_mode:
+            for i in range(4):
+                print("._____._____._____._____.")
+                line = ""
+                for j in range(4):
+                    piece = str(self.board[i][j])
+                    if piece == "16":
+                        piece = "  "
+
+                    if len(piece) == 2:
+                        if j == 3:
+                            line += "|  "+piece+" |"
+                        else:
+                            line += "|  "+piece+" "
+                    else:
+                        if j == 3:
+                            line += "|  "+piece+"  |"
+                        else:
+                            line += "|  "+piece+"  "
+                print(line)
+            print("._____._____._____._____.")
+        else:
+            print(self.board)
