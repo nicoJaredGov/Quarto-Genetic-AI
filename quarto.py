c406776a6b01964392da246ed694ddb9bca4f069
Nico Govindsamy <ngovindsamy18@gmail.com>
2023-07-12 18:09:15 +0200

    Added state information and modified setBoard method to update game state accordingly

--- quarto.py
+++ quarto.py
@@ -6,12 +6,29 @@ class QuartoGame:
         self.board = np.full((4,4), 16)
         self.gui_mode = gui_mode
 
+    #experimental method to allow any board to be loaded and simulated from that point
+    #NOTE 1 First move function has to be called again to set the current player's piece to place
+    #NOTE 2 There is obviously no game history. Only history after the board is loaded can be recorded.
     def setBoard(self, board_encoding=None):
         #no board encoding passed, therefore clear board
         if board_encoding is None:
             self.board = np.full((4,4), 16)
+            self.availablePieces = set(range(16))
+            self.availablePositions = set(range(16))
+            
         else:
-            self.board = qutil.decodeBoard(board_encoding)
+            board_array = [int(board_encoding[i]+board_encoding[i+1]) for i in range(0,len(board_encoding),2)]
+
+            #determine available positions
+            self.availablePositions = set([i for i, e in enumerate(board_array) if e == 16])
+
+            #determine which pieces are still available
+            self.availablePieces = set(board_array)
+            if 16 in self.availablePieces:
+                self.availablePieces.remove(16)
+            self.availablePieces = set(range(16)) - self.availablePieces
+  
+            self.board = np.reshape(board_array, (4,4))
     
     def encodeBoard(self):
         return qutil.encodeBoard(self.board)
