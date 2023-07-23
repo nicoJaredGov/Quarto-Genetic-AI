661d8a75c33f7a2c2ec48ac118cb1da9416421b5
Nico Govindsamy <ngovindsamy18@gmail.com>
2023-07-23 16:29:49 +0200

    Random Quarto Agent - makes random legal moves

--- quarto_agents.py
+++ quarto_agents.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import Any
+from numpy import random
 
 class GenericQuartoAgent(ABC):
     
@@ -20,4 +21,16 @@ class HumanPlayer(GenericQuartoAgent):
     def makeMove(self, quartoGameState):
         position = int(input("Cell: "))
         nextPiece = int(input("Your opponent's next piece: "))
+        return position, nextPiece
+
+class RandomQuartoAgent(GenericQuartoAgent):
+
+    def makeFirstMove(self, quartoGameState):
+        nextPiece = random.choice(list(quartoGameState[2]))
+        return nextPiece
+    
+    def makeMove(self, quartoGameState):
+        position = random.choice(list(quartoGameState[3]))
+        nextPiece = random.choice(list(quartoGameState[2]))
+        print(f"Random agent placed piece at cell {position} and nextPiece is {nextPiece}")
         return position, nextPiece
