9d02dbcb0f55fe8b42a775598a95e12c095b750b
Nico Govindsamy <ngovindsamy18@gmail.com>
2023-08-07 19:59:44 +0200

    Fixed indentation

--- quarto_util.py
+++ quarto_util.py
@@ -1,16 +1,16 @@
 import numpy as np
 
 def encodeBoard(board_array):
-        encoding = ""
-        for i in range(4):
-            for j in range(4):
-                num = board_array[i][j]
-                if num <= 9:
-                    encoding += "0"+str(num)
-                else:
-                    encoding += str(num)
-        
-        return encoding
+    encoding = ""
+    for i in range(4):
+        for j in range(4):
+            num = board_array[i][j]
+            if num <= 9:
+                encoding += "0"+str(num)
+            else:
+                encoding += str(num)
+    
+    return encoding
     
 def decodeBoard(encoding):
     board_array = [int(encoding[i]+encoding[i+1]) for i in range(0,len(encoding),2)]
