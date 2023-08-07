24b780e27f22fc325876d42452761a1536bccd98
Nico Govindsamy <ngovindsamy18@gmail.com>
2023-08-07 22:57:01 +0200

    Template for negamax agent

--- quarto_agents.py
+++ quarto_agents.py
@@ -1,6 +1,9 @@
 from abc import ABC, abstractmethod
 from typing import Any
 from numpy import random
+import numpy as np
+import itertools
+import quarto_util as qutil
 
 class GenericQuartoAgent(ABC):
     
@@ -34,3 +37,26 @@ class RandomQuartoAgent(GenericQuartoAgent):
         nextPiece = random.choice(list(quartoGameState[2]))
         print(f"Random agent placed piece at cell {position} and nextPiece is {nextPiece}")
         return position, nextPiece
+   
+#NegaMax with Alpha-Beta pruning   
+class NegamaxAgent(GenericQuartoAgent):
+
+    def makeFirstMove(self, quartoGameState):
+        nextPiece = random.choice(list(quartoGameState[2]))
+        return nextPiece
+    
+    def makeMove(self, quartoGameState):
+        position = random.choice(list(quartoGameState[3]))
+        nextPiece = random.choice(list(quartoGameState[2]))
+        print(f"Random agent placed piece at cell {position} and nextPiece is {nextPiece}")
+        return position, nextPiece
+    
+    def alphaBeta(self, board, availablePositions, availableNextPieces, depth, alpha, beta):
+        if depth == 0 or qutil.isGameOver(board) or len(availablePositions) == 0:
+            return self.evaluation(board)
+        
+        score = -np.inf
+        for move in itertools.product(availablePositions, availableNextPieces):
+            
+
+    def evaluation(self, boardArray):
